5d4f45566be1b05edc13091e065d985df9208ede
Hououin Kyouma <inwazzin@gmail.com>
2022-03-04 23:45:20 +0100

    create context utility

---
+++ src/dedicated/components/App/Main/Tomography/context/context.ts
@@ -0,0 +1,14 @@
+import { createContext } from "@/shared/utils";
+import { createSignal } from "solid-js";
+
+export const [useTomography, TomographyProvider] = createContext(
+  "Tomography",
+  () => {
+    const [image, setImage] = createSignal("scout.jpg");
+
+    return {
+      image,
+      setImage,
+    } as const;
+  }
+);
---
+++ src/shared/utils/createContext/createContext.tsx
@@ -0,0 +1,29 @@
+import {
+  Component,
+  createContext as createSolidContext,
+  useContext,
+} from "solid-js";
+export * from "./createContext";
+
+export const createContext = <Props extends object, State extends object>(
+  name: string,
+  provider: ((props: Props) => State) | (() => State)
+): readonly [() => State, Component<Props>] => {
+  const Context = createSolidContext<State>();
+
+  const hook = () => {
+    const context = useContext(Context);
+    if (context === undefined) {
+      throw new Error(`Context must be used within ${name} Provider`);
+    }
+    return context;
+  };
+
+  const Provider: Component<Props> = ({ children, ...props }) => (
+    <Context.Provider value={provider(props as any)}>
+      <span>{children}</span>
+    </Context.Provider>
+  );
+
+  return [hook, Provider] as const;
+};
