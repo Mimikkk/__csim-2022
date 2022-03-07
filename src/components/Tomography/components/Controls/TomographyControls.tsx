57cc5528191b3417b9d9e87a59a57cb4366d6d01
Hououin Kyouma <inwazzin@gmail.com>
2022-03-07 02:55:09 +0100

    tomography input layout

--- src/components/Tomography/components/Controls/TomographyControls.tsx
+++ src/components/Tomography/components/Controls/TomographyControls.tsx
@@ -18,31 +18,33 @@ export const TomographyParameters = () => {
   const { image } = useTomography();
 
   return (
-    <fieldset disabled={!image()}>
-      <Range
-        default={90}
-        min={90}
-        max={720}
-        step={90}
-        label="Liczba detekorów"
-        onChange={setDetectors}
-      />
-      <Range
-        default={90}
-        min={90}
-        max={720}
-        step={90}
-        label="Liczba skanów"
-        onChange={setScans}
-      />
-      <Range
-        default={45}
-        min={45}
-        max={270}
-        step={45}
-        label="Rozpiętość"
-        onChange={setAngle}
-      />
+    <fieldset disabled={!image()} class="flex flex-col gap-y-2">
+      <div>
+        <Range
+          default={90}
+          min={90}
+          max={720}
+          step={90}
+          label="Detekory"
+          onChange={setDetectors}
+        />
+        <Range
+          default={90}
+          min={90}
+          max={720}
+          step={90}
+          label="Skany"
+          onChange={setScans}
+        />
+        <Range
+          default={45}
+          min={45}
+          max={270}
+          step={45}
+          label="Rozpiętość"
+          onChange={setAngle}
+        />
+      </div>
       <Checkbox
         default={false}
         label="Filtrowanie"
