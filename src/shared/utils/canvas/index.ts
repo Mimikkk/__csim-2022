04aa809a2c24fb724a6d29dc11ac9de88cb9db41
Hououin Kyouma <inwazzin@gmail.com>
2022-03-09 22:24:58 +0100

    indices

---
+++ src/shared/utils/canvas/index.ts
@@ -0,0 +1,2 @@
+export * from "./createPixelHandler";
+export * from "./eachpixel";
---
+++ src/shared/utils/canvas/types.ts
@@ -0,0 +1,22 @@
+export type Point = [number, number];
+export type Rgba = [number, number, number, number];
+
+export interface HTMLCanvasCallback {
+  (point: Point, rgba?: Rgba): Rgba;
+}
+
+export interface CanvasPixelHandlerCallback {
+  (
+    canvas: HTMLCanvasElement,
+    timestamp: DOMHighResTimeStamp
+  ): HTMLCanvasCallback;
+}
+
+export interface CanvasRefCallback {
+  (canvas: HTMLCanvasElement): void;
+}
+
+export interface CanvasLoopCallback {
+  (loop: CanvasPixelHandlerCallback): CanvasRefCallback;
+}
+
