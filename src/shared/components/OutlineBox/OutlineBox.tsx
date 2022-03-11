58414a620dc280a9c995fc357dc0b938950dd8ec
Hououin Kyouma <inwazzin@gmail.com>
2022-03-12 00:06:28 +0100

    outline box labels

--- src/shared/components/OutlineBox/OutlineBox.tsx
+++ src/shared/components/OutlineBox/OutlineBox.tsx
@@ -1,13 +1,26 @@
 import { Component } from "solid-js";
 import "./OutlineBox.scss";
 import cx from "classnames";
+import { Show } from "solid-js";
 
 interface Props {
   class?: string;
   centered?: boolean;
+  label?: string;
 }
 export const OutlineBox: Component<Props> = ({
   children,
   centered = false,
+  label,
   class: classname,
-}) => <div class={cx("outline-box", { centered }, classname)}>{children}</div>;
+}) => {
+  return (
+    <fieldset
+      class={cx("outline-box", { centered, label: Boolean(label) }, classname)}>
+      <Show when={label}>
+        <legend>{label}</legend>
+      </Show>
+      {children}
+    </fieldset>
+  );
+};
