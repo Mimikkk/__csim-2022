96c31c5659463e9ac166b3981bbdd2f42af92823
Hououin Kyouma <inwazzin@gmail.com>
2022-03-12 14:43:13 +0100

    img_to_array util

--- server/src/utils/__init__.py
+++ server/src/utils/__init__.py
@@ -1,1 +1,1 @@
-from .image_conversion import img_to_base64, base64_to_img
+from .image_conversion import img_to_base64, base64_to_img, img_to_array
--- server/src/utils/image_conversion.py
+++ server/src/utils/image_conversion.py
@@ -3,6 +3,8 @@ from base64 import b64encode, b64decode
 from PIL.Image import Image, open, new
 from PIL import UnidentifiedImageError
 from io import BytesIO
+from numpy.typing import NDArray
+from numpy import array
 
 def img_to_base64(image: Image) -> str:
   image.save(buffered := BytesIO(), format="PNG")
@@ -13,3 +15,7 @@ def base64_to_img(base64: str) -> Image:
     return open(BytesIO(b64decode(base64)))
   except UnidentifiedImageError:
     return new("RGB", (1, 1))
+
+def img_to_array(image: Image) -> NDArray:
+  # noinspection PyTypeChecker
+  return array(image)
