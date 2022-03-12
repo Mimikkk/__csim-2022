9582f8ff44d284db38315e54deeaeb12f1f2e0d0
Hououin Kyouma <inwazzin@gmail.com>
2022-03-12 01:43:31 +0100

    image_conversion.py

--- server/src/utils/image_conversion.py
+++ server/src/utils/image_conversion.py
@@ -1,11 +1,11 @@
 from base64 import b64encode, b64decode
-from PIL.Image import Image, open
+
+from PIL.Image import Image, open, new
 from io import BytesIO
 
 def img_to_base64(image: Image) -> str:
-  buffered = BytesIO()
-  image.save(buffered, format="PNG")
+  image.save(buffered := BytesIO(), format="PNG")
   return str(b64encode(buffered.getvalue()))
 
 def base64_to_img(base64: str) -> Image:
-  return open(BytesIO(b64decode(base64)))
+  return orr(lambda: open(BytesIO(b64decode(base64))), new("RGB", (1, 1)))
