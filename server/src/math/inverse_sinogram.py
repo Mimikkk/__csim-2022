7a61c9c029edbc8ce773bdd689659195cbeb1744
Hououin Kyouma <inwazzin@gmail.com>
2022-03-12 19:33:57 +0100

    inverse send data

--- server/src/math/inverse_sinogram.py
+++ server/src/math/inverse_sinogram.py
@@ -4,7 +4,7 @@ from PIL.ImageOps import grayscale as to_grayscale
 from src.utils import img_to_array
 
 
-def inverse_sinogram(sinogram: Image) -> Image:
+def inverse_sinogram(sinogram: Image, scans: int, detectors: int, angle: float, use_filter: bool) -> Image:
   grayscale = img_to_array(to_grayscale(sinogram))
+  return fromarray(grayscale, 'L').convert("RGBA")
 
-  return fromarray(grayscale, 'L')
