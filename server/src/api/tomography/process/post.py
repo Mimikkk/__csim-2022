58c280aa365b654ee9b10f73babd4999887846d0
Hououin Kyouma <inwazzin@gmail.com>
2022-03-13 14:49:00 +0100

    fix rmse

--- server/src/api/tomography/process/post.py
+++ server/src/api/tomography/process/post.py
@@ -1,16 +1,16 @@
 from src import app
 from src.math import rmse, create_sinogram, inverse_sinogram
-from src.utils import img_to_base64, img_to_array
+from src.utils import img_to_base64, img_to_array, square_image
 from .models import TomographyResponse, TomographyRequest
 
 @app.post("/api/tomography/process", response_model=TomographyResponse)
 def process_command(item: TomographyRequest):
   original = item.image
-  sinogram = create_sinogram(original, item.detectors, item.scans, item.angle, item.use_filter)
-  inverse = inverse_sinogram(sinogram, item.detectors, item.scans, item.angle, item.use_filter)
+  (sinogram, radius) = create_sinogram(original, item.detectors, item.scans, item.angle, item.use_filter)
+  inverse = inverse_sinogram(sinogram, radius, item.detectors, item.scans, item.angle)
 
   return TomographyResponse(
     img_to_base64(inverse),
     img_to_base64(sinogram),
-    rmse(img_to_array(sinogram), img_to_array(inverse))
+    rmse(img_to_array(square_image(original)), img_to_array(inverse))
   )
