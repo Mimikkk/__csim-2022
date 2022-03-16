2d9b3e1134c399c952c8fbb14adba78439f0a36c
Hououin Kyouma <inwazzin@gmail.com>
2022-03-16 13:40:02 +0100

    debuggingi

--- server/src/api/tomography/process/post.py
+++ server/src/api/tomography/process/post.py
@@ -2,16 +2,19 @@ from typing import Any
 from PIL.Image import fromarray
 from numpy import ndarray, around, clip
 import numpy as np
+from sklearn.preprocessing import normalize
+
 from src import app
 from src.math import rmse, create_sinogram, inverse_sinogram, create_sinogram_filter, create_sinogram_filter_kernel
 from src.utils import img_to_base64, img_to_array, square_image
 from .models import TomographyResponse, TomographyRequest
 from PIL.ImageOps import grayscale as to_grayscale
 
+def clip_array(arr: ndarray[(Any, Any), int], feature_range: (float, float)) -> ndarray[(Any, Any), int]:
+  return clip(around(arr / arr.max(initial=None) * 255), *feature_range)
+
 def array_to_base64(arr: ndarray[(Any, Any), int]) -> str:
-  return img_to_base64(
-    fromarray(clip(around(arr / arr.max(initial=None) * 255), 0, 255).astype(np.int8), 'L').convert("RGBA")
-  )
+  return img_to_base64(fromarray(arr.astype(np.int8), 'L').convert("RGBA"))
 
 @app.post("/api/tomography/process", response_model=TomographyResponse)
 def process_command(item: TomographyRequest):
@@ -26,7 +29,7 @@ def process_command(item: TomographyRequest):
   reconstruction = inverse_sinogram(sinogram, radius, item.scans, item.detectors, item.spread)
 
   return TomographyResponse(
-    array_to_base64(reconstruction),
+    array_to_base64(clip_array(reconstruction, (0, 255))),
     array_to_base64(sinogram),
     rmse(grayscale, reconstruction)
   )
--- src/components/Tomography/components/Controls/context/context.ts
+++ src/components/Tomography/components/Controls/context/context.ts
@@ -2,9 +2,9 @@ import { createContext } from "@/shared/utils";
 import { createSignal } from "solid-js";
 
 export const [useControls, ControlsProvider] = createContext("Controls", () => {
-  const [spread, setSpread] = createSignal(135);
-  const [detectors, setDetectors] = createSignal(180);
-  const [scans, setScans] = createSignal(270);
+  const [spread, setSpread] = createSignal(25);
+  const [detectors, setDetectors] = createSignal(25);
+  const [scans, setScans] = createSignal(15);
   const [useFilter, setUseFilter] = createSignal(true);
 
   return {
