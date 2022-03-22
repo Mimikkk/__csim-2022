82413088aeed7cc29388d0a0d7cc29d7e2a5714a
Hououin Kyouma <inwazzin@gmail.com>
2022-03-22 21:43:02 +0100

    array post format

--- server/src/utils/image_conversion.py
+++ server/src/utils/image_conversion.py
@@ -5,11 +5,21 @@ from PIL.Image import Image, open, new
 from PIL import UnidentifiedImageError
 from io import BytesIO
 
-from numpy import array, subtract, ndarray, number
+from imageio import imwrite
+from numpy import array, subtract, ndarray, number, interp, uint8
+
+def bytes_to_base64(bytes: bytes) -> str:
+  return f"data:image/png;base64,{str(b64encode(bytes), 'utf-8')}"
 
 def img_to_base64(image: Image) -> str:
   image.save(buffered := BytesIO(), format="PNG")
-  return f"data:image/png;base64,{str(b64encode(buffered.getvalue()), 'utf-8')}"
+  return bytes_to_base64(buffered.getvalue())
+
+def array_to_base64(arr: ndarray[(Any, Any, Any), int]) -> str:
+  arr = arr / arr.max(initial=None)
+  ranges = (arr.min(initial=None), arr.max(initial=None))
+  imwrite(buffered := BytesIO(), interp(arr, ranges, (0, 255)).astype(uint8), format='png')
+  return bytes_to_base64(buffered.getvalue())
 
 def base64_to_img(base64: str) -> Image:
   try:
