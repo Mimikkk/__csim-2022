80c32c3b5876373039b6aa6dc642539c07556b39
Hououin Kyouma <inwazzin@gmail.com>
2022-03-27 15:49:08 +0200

    fihr restub

--- src/components/Fihr/Fihr.tsx
+++ src/components/Fihr/Fihr.tsx
@@ -1,8 +1,1 @@
-import { createContext } from "@/shared/utils";
-export const [useContext, ContextProvider] = createContext("Tomography", () => {
-  return {};
-});
-
-export const Fihr = () => {
-  return <p>todo - 14.06.2022</p>;
-};
+export const Fihr = () => <p>todo - 14.06.2022</p>;
