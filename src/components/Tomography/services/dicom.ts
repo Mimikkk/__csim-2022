6e23e9c59d47aaf56e0017cab3275f15e1c71c71
Hououin Kyouma <inwazzin@gmail.com>
2022-03-27 18:00:38 +0200

    load response endpoint handle

--- src/components/Tomography/services/dicom.ts
+++ src/components/Tomography/services/dicom.ts
@@ -1,8 +1,27 @@
 import axios from "axios";
+import { DataResponse, parseResponse } from "@/shared/utils";
 
 const ApiUrl = "http://localhost:3001/api/tomography/dicom";
 
+export interface Patient {
+  id: string;
+  name: string;
+  comments: string;
+}
+
+interface Load {
+  image: string;
+  patient: Patient;
+}
+
 export const dicomService = {
   save: (item) => axios.post<FormData>(`${ApiUrl}/save`, item),
-  load: (item) => axios.post<FormData>(`${ApiUrl}/load`, item),
+  load: (item: File | Blob) => {
+    const form = new FormData();
+    form.append("file", item);
+
+    return axios
+      .post<FormData, DataResponse<Load>>(`${ApiUrl}/load`, form)
+      .then(parseResponse);
+  },
 };
