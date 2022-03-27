ea21e853737129f8c76850a596e5e20f3cb4f029
Hououin Kyouma <inwazzin@gmail.com>
2022-03-27 15:47:48 +0200

    add dropabble button

--- src/shared/components/Button/Button.tsx
+++ src/shared/components/Button/Button.tsx
@@ -1,34 +1,57 @@
 import { Component } from "solid-js";
 import "./Button.scss";
+import cx from "classnames";
 
 interface Props {
-  onClick: () => void;
+  onClick?: () => void;
+  onDrop?: (file?: File) => void;
+  disabled?: boolean;
 }
 
 const createRipple = (
   { clientX, clientY }: MouseEvent,
-  button: HTMLElement
+  button: HTMLElement,
+  color?: "white" | "green"
 ) => {
   let [x, y] = [clientX - button.offsetLeft, clientY - button.offsetTop];
   let ripple = document.createElement("span");
   ripple.style.top = `${y}px`;
   ripple.style.left = `${x}px`;
-  ripple.className = "ripple";
+  ripple.className = cx("ripple", color && `ripple--${color}`);
 
   button.append(ripple);
   setTimeout(() => ripple.remove(), 1000);
 };
 
-export const Button: Component<Props> = ({ onClick, children }) => {
+export const Button: Component<Props> = ({
+  onClick,
+  disabled,
+  onDrop,
+  children,
+}) => {
   let button: HTMLButtonElement = null;
 
   return (
     <button
+      type="button"
       class="Button"
       ref={button}
-      onClick={(event) => {
+      disabled={disabled}
+      ondragover={(event) => {
+        if (!onDrop) return;
+        event.preventDefault();
         createRipple(event, button);
-        onClick();
+      }}
+      ondrop={(event) => {
+        if (!onDrop) return;
+        event.preventDefault();
+        createRipple(event, button, "green");
+        onDrop(event.dataTransfer.files[0]);
+      }}
+      onClick={(event) => {
+        if (!onClick) return;
+        createRipple(event, button);
+        onClick?.();
       }}>
       {children}
     </button>
