5a2fe50b5ef80bb4d08205ae602adbf03a2301f5
Hououin Kyouma <inwazzin@gmail.com>
2022-03-28 18:16:01 +0200

    redo range as props oriented per solidjs specification

--- src/shared/components/inputs/Range/Range.tsx
+++ src/shared/components/inputs/Range/Range.tsx
@@ -3,43 +3,31 @@ import "./Range.scss";
 
 interface Props {
   label?: string;
-  default?: number;
+  value?: number;
   min: number;
   max: number;
   step: number;
   onChange?: (value: number) => void;
 }
 
-export const Range: Component<Props> = ({
-  label,
-  onChange,
-  min,
-  max,
-  step,
-  default: defaultValue,
-}) => {
-  const [value, setValue] = createSignal(defaultValue || min);
-
-  return (
-    <label>
-      <fieldset class="range-fieldset">
-        <legend>
-          {label} - {value() as any}
-        </legend>
-        {min}
-        <input
-          type="range"
-          step={step}
-          min={min}
-          max={max}
-          value={defaultValue}
-          onChange={(event) => {
-            onChange(event.currentTarget.value as any);
-            setValue(event.currentTarget.value as any);
-          }}
-        />
-        {max}
-      </fieldset>
-    </label>
-  );
-};
+export const Range: Component<Props> = (props) => (
+  <label>
+    <fieldset class="range-fieldset">
+      <legend>
+        {props.label} - {props.value}
+      </legend>
+      {props.min}
+      <input
+        type="range"
+        step={props.step}
+        min={props.min}
+        max={props.max}
+        value={props.value}
+        onChange={(event) => {
+          props?.onChange(event.currentTarget.value as any);
+        }}
+      />
+      {props.max}
+    </fieldset>
+  </label>
+);
