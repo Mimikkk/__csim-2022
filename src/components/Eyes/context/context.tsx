94f255ba7867cded01d4994a67869b85aed34b7d
Hououin Kyouma <inwazzin@gmail.com>
2022-04-19 07:36:22 +0200

    hookup services clientside

--- src/components/Eyes/context/context.tsx
+++ src/components/Eyes/context/context.tsx
@@ -2,28 +2,23 @@ import { createContext } from "@/shared/utils";
 import { createSignal } from "solid-js";
 import { Nullable } from "@/shared/types";
 import { createTracked } from "@/shared/hooks";
-import { timeout } from "@/shared/utils/timeout";
+import {
+  cnnService,
+  traditionalService,
+  knnService,
+} from "@/components/Eyes/services";
 
 export const [useControls, ControlsProvider] = createContext("Controls", () => {
   const [original, setOriginal] = createSignal<Nullable<string>>(null);
   const [veins, setVeins] = createSignal<Nullable<string>>(null);
   const [traditional, traditionalStatus, createTraditional] = createTracked({
-    fn: async () => {
-      await timeout(500);
-      return original();
-    },
+    fn: () => traditionalService.process(original()),
   });
   const [knn, knnStatus, createKnn] = createTracked({
-    fn: async () => {
-      await timeout(500);
-      return original();
-    },
+    fn: () => knnService.process(original()),
   });
   const [cnn, cnnStatus, createCnn] = createTracked({
-    fn: async () => {
-      await timeout(500);
-      return original();
-    },
+    fn: () => cnnService.process(original()),
   });
 
   return {
