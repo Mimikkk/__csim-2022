11fc1de2167dc51456a04d57ae2f6340cca2a73a
Hououin Kyouma <inwazzin@gmail.com>
2022-04-19 07:47:05 +0200

    update data signatures in apis

--- src/components/Eyes/services/traditonal.ts
+++ src/components/Eyes/services/traditonal.ts
@@ -1,10 +1,12 @@
 import { createApiUrl } from "@/shared/services";
 import axios from "axios";
-import { DataResponse } from "@/shared/utils";
+import { DataResponse, parseResponse } from "@/shared/utils";
 
 const ApiUrl = createApiUrl("eyes/traditional");
 
 export const traditionalService = {
   process: (image: string) =>
-    axios.post<string, DataResponse<string>>(`${ApiUrl}/process`, image),
+    axios
+      .post<string, DataResponse<string>>(`${ApiUrl}/process`, { image })
+      .then(parseResponse),
 };
