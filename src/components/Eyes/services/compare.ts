954146cab19e5af2db124f8d655e6e0ebb70be0b
Hououin Kyouma <inwazzin@gmail.com>
2022-04-20 18:28:56 +0200

    create compare service

---
+++ src/components/Eyes/services/compare.ts
@@ -0,0 +1,27 @@
+import { createApiUrl } from "@/shared/services";
+import axios from "axios";
+import { DataResponse, parseResponse } from "@/shared/utils";
+
+const ApiUrl = createApiUrl("eyes");
+
+interface CompareRequest {
+  processed: string;
+  expert: string;
+}
+
+interface CompareStatistics {
+  confusion: string;
+  specificity: number;
+  sensitivity: number;
+  accuracy: number;
+}
+
+export const compareService = {
+  compare: (processed: string, expert: string) =>
+    axios
+      .post<CompareRequest, DataResponse<CompareStatistics>>(
+        `${ApiUrl}/compare`,
+        { processed, expert }
+      )
+      .then(parseResponse),
+};
