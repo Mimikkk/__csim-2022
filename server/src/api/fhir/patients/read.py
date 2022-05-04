4d3d159e97368c1e06c8afb156b9cfdc2562ccdb
Hououin Kyouma <inwazzin@gmail.com>
2022-05-04 17:52:06 +0200

    server patient resource reader

--- server/src/api/fhir/patients/read.py
+++ server/src/api/fhir/patients/read.py
@@ -1,12 +1,33 @@
-from fhirpy.base import SyncSearchSet
+import json
 
 from src import fhir, app, logger
 from src.utils import first
+AllowedResources = ['Patient', 'Observation', 'MedicationStatement']
 
-@app.post("/api/fhir/patients/read")
+def is_patient(x): return x['resourceType'] == 'Patient'
+def is_observation(x): return x['resourceType'] == 'Observation'
+def is_medication_statement(x): return x['resourceType'] == 'MedicationStatement'
+
+@app.get("/api/fhir/patients/read")
 async def patients_read_get(id: str):
   logger.info(f"Reading patient with id: '{id}'...")
 
-  searchset: SyncSearchSet = fhir.resource("Patient")
+  executedata = fhir.execute(f"Patient/{id}/$everything")
 
-  return first(searchset.search(_id=id).fetch())
+  entries = executedata.entry
+  entries = [entry.resource for entry in entries if entry.resource.resourceType in AllowedResources]
+
+  x = {
+    "patient": first(list(filter(is_patient, entries))),
+    "observations": list(filter(is_observation, entries)),
+    "medication_statements": list(filter(is_medication_statement, entries))
+  }
+
+  print("Had Observations", len(x['observations']) > 0)
+  json.dump(x, open(f"search-response.ts", "w"), indent=2)
+
+  return json.dumps({
+    "patient": first(list(filter(is_patient, entries))),
+    "observations": list(filter(is_observation, entries)),
+    "medication_statements": list(filter(is_medication_statement, entries))
+  }, indent=2)
--- server/src/api/fhir/patients/search.py
+++ server/src/api/fhir/patients/search.py
@@ -6,7 +6,7 @@ from fhirpy.base import SyncSearchSet
 Gender = Literal['unknown', 'female', 'male', 'other']
 
 @app.get("/api/fhir/patients/search")
-async def patients_search_get(name: str = "", gender: Gender = "", limit: int = 10):
+async def patients_search_get(name: str = "", gender: Gender = "", limit: int = 20):
   searchset: SyncSearchSet = fhir.resources("Patient")
 
   patients = searchset.search(name__contains=name, gender=gender).limit(limit).fetch()
