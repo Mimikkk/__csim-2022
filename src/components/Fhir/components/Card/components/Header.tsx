e3858b7e6e4c1a0c39749ed74eb96d8c636d9a49
Hououin Kyouma <inwazzin@gmail.com>
2022-05-10 01:52:17 +0200

    create header part

---
+++ src/components/Fhir/components/Card/components/Header.tsx
@@ -0,0 +1,26 @@
+import { useData } from "solid-app-router";
+import { ReadResponse } from "@/components/Fhir/services";
+import { Patient } from "@/components/Fhir/models";
+import { Tracked } from "@/shared/hooks";
+
+export const CardHeader = () => {
+  const [data] = useData<Tracked<ReadResponse>>();
+  const { birthdate, gender, identifierTitle, fullname } = Patient.Row;
+  const { patient } = data();
+
+  return (
+    <header class="w-full bg-rose-900 gap-2 items-center opacity-60">
+      <span class="w-full capitalize animated">{fullname(patient)}</span>
+      <span>/</span>
+      <span class="w-full capitalize animated">{gender(patient)}</span>
+      <span>/</span>
+      <span class="w-full capitalize animated">{birthdate(patient)}</span>
+      <span>/</span>
+      <span
+        class="w-full capitalize animated ellipsis"
+        title={identifierTitle(patient)}>
+        {identifierTitle(patient)}
+      </span>
+    </header>
+  );
+};
